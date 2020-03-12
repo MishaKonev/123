7675f0bad6b50687b5ffdb67029b48ecb591e9bf
misha123123123 <36543528+misha123123123@users.noreply.github.com>
2020-03-12 16:25:33 +0200

    Update Program.cs

--- FirstCommit/FirstCommit/Program.cs
+++ FirstCommit/FirstCommit/Program.cs
@@ -10,7 +10,7 @@ namespace FirstProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!123");
         }
     }
 }
